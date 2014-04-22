97ae857a859f41c46f76b0461615db86aba28c67
ClemencioMorales <clemencio.morales.lucas@alumnos.upm.es>
2014-04-22 12:25:54 +0200

    Implementación  de la GUI básica de la historia "Crear Proyecto" en JSF. Funcionamiento: Se ejecuta "createProject.xhtml", se introduce un nombre para el proyecto y este se recibirá en "responseCreateProject.xhtml". Para almacenar el nombre se utiliza "ProjectBean.java"

---
+++ src/main/java/eui/miw/pfm/controllers/beans/ProjectBean.java
@@ -0,0 +1,21 @@
+package eui.miw.pfm.controllers.beans;
+ 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import java.io.Serializable;
+ 
+@ManagedBean
+@SessionScoped
+public class ProjectBean extends Bean implements Serializable {
+ 
+	private String projectName;
+
+	public String getProjectName() {
+		return projectName;
+	}
+
+	public void setProjectName(String projectName) {
+		this.projectName = projectName;
+	}
+
+}
