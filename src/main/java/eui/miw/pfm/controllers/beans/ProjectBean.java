c736c6eb2ca2dc3a76bcb53ad6a00a655cd1c8cf
ClemencioMorales <clemencio.morales.lucas@alumnos.upm.es>
2014-04-23 13:07:59 +0200

    GUI asociada  a la tarea "Crear Vistas" en la historia "Abrir Proyecto".Vista "openProject.xhtml" y actualizaciones en "ProjectBean.java"

--- src/main/java/eui/miw/pfm/controllers/beans/ProjectBean.java
+++ src/main/java/eui/miw/pfm/controllers/beans/ProjectBean.java
@@ -1,26 +1,62 @@
 package eui.miw.pfm.controllers.beans;
- 
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import java.io.Serializable;
- 
+
 /**
  *
  * @author Clemencio Morales
  */
-
 @ManagedBean
 @SessionScoped
 public class ProjectBean extends Bean implements Serializable {
- 
-	private String projectName;
 
-	public String getProjectName() {
-		return projectName;
-	}
+    private String projectName;
+    private int currentIteration;
+    private int numberOfIterations;//NOPMD
+    private String description;
+    private List<String> participants = new ArrayList<>();
 
-	public void setProjectName(String projectName) {
-		this.projectName = projectName;
-	}
+    public String getProjectName() {
+        return projectName;
+    }
 
+    public void setProjectName(String projectName) {//NOPMD
+        this.projectName = projectName;
+    }
+
+    public int getCurrentIteration() {
+        return currentIteration;
+    }
+
+    public void setCurrentIteration(int currentIteration) {
+        this.currentIteration = currentIteration;
+    }
+
+    public int getNumberOfIterations() {
+        return numberOfIterations;
+    }
+
+    public void setNumberOfIterations(int numberOfIterations) {
+        this.numberOfIterations = numberOfIterations;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(List<String> participants) {
+        this.participants = participants;
+    }
 }
