4048e1f8db2329da59728e67ad8d435b24471db5
ClemencioMorales <clemencio.morales.lucas@alumnos.upm.es>
2014-04-29 09:53:27 +0200

    Refactorización de UseCaseBean

--- src/main/java/eui/miw/pfm/controllers/beans/UseCaseBean.java
+++ src/main/java/eui/miw/pfm/controllers/beans/UseCaseBean.java
@@ -7,14 +7,19 @@
 package eui.miw.pfm.controllers.beans;
 
 import eui.miw.pfm.controllers.ejb.UseCaseEjb;
+import eui.miw.pfm.models.entities.ProjectEntity;
 import eui.miw.pfm.models.entities.UseCaseEntity;
+import eui.miw.pfm.util.SessionMap;
 import java.io.Serializable;
+import java.util.logging.Logger;
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Named;
 
 /**
  *
  * @author Roberto Amor
+ * @author Clemencio Morales
+ * @author Manuel Rodríguez
  */
 @SessionScoped
 @Named
@@ -22,9 +27,20 @@ public class UseCaseBean extends Bean implements Serializable {
     
     private static final long serialVersionUID = 1L;
     private UseCaseEntity usecase;
+    private ProjectEntity project;
+    private SessionMap sessionMap;
+    private static final Logger LOG = Logger.getLogger(ConfProjectBean.class.getName());//NOPMD
     
     public UseCaseBean(){
         super();
+        project = new ProjectEntity();
+        this.sessionMap = new SessionMap();
+
+        try {
+            this.project = ((ProjectEntity) this.sessionMap.get("project"));
+        } catch (Exception e) {
+            LOG.warning("No session exist");
+        }
     }
     
     public UseCaseBean(final UseCaseEntity usecase){
@@ -53,6 +69,7 @@ public class UseCaseBean extends Bean implements Serializable {
         ejb.create(this.usecase);
         result = "list_usecases";
         return result;
+        
     }
     
     public String delete(){
