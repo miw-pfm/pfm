da6c6e12ecdf4e65f0721676b4df1d70b222a774
mrrm <manuel.rodriguez@pucp.pe>
2014-04-30 12:05:36 +0200

    Refactor de pruebas de caso de uso, integración de prueba crear y actualizar en una clase

--- src/test/java/eui/miw/pfm/AllTest.java
+++ src/test/java/eui/miw/pfm/AllTest.java
@@ -6,9 +6,9 @@
 package eui.miw.pfm;
 
 import eui.miw.pfm.controllers.beans.TestConfProjectBean;
-import eui.miw.pfm.controllers.ejb.TestNameProjectValidator;
 import eui.miw.pfm.controllers.ejb.TestCreateProject;
-import eui.miw.pfm.controllers.ejb.TestCreateUseCase;
+import eui.miw.pfm.controllers.ejb.TestNameProjectValidator;
+import eui.miw.pfm.controllers.ejb.TestUseCase;
 import eui.miw.pfm.models.entities.UserEntityTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
@@ -18,7 +18,7 @@ import org.junit.runners.Suite;
  * @author bk0823
  */
 @RunWith(Suite.class)
-@Suite.SuiteClasses({TestConfProjectBean.class, TestNameProjectValidator.class, TestCreateProject.class, UserEntityTest.class, TestCreateUseCase.class})
+@Suite.SuiteClasses({TestConfProjectBean.class, TestNameProjectValidator.class, TestCreateProject.class, UserEntityTest.class, TestUseCase.class})
 public class AllTest {
 
 }
--- src/test/java/eui/miw/pfm/controllers/ejb/TestUseCase.java
+++ src/test/java/eui/miw/pfm/controllers/ejb/TestUseCase.java
@@ -9,8 +9,14 @@ package eui.miw.pfm.controllers.ejb;
 import eui.miw.pfm.models.dao.AbstractDAOFactory;
 import eui.miw.pfm.models.dao.interfaces.ProjectDAO;
 import eui.miw.pfm.models.dao.interfaces.UseCaseDAO;
+import eui.miw.pfm.models.dao.interfaces.UserDAO;
+import eui.miw.pfm.models.entities.ProjectEntity;
 import eui.miw.pfm.models.entities.UseCaseEntity;
+import eui.miw.pfm.models.entities.UserEntity;
+import java.util.Date;
+import org.junit.After;
 import org.junit.Assert;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
@@ -23,27 +29,98 @@ public class TestUseCase {
     
     private transient UseCaseEntity usecase;
     private transient UseCaseEjb usecaseEjb;
+    private transient ProjectDAO projectDAO;
+    private transient UseCaseDAO usecaseDAO;
+    private transient UserDAO userDAO;
+    private transient UserEntity user;
+    private transient ProjectEntity project;
+     private transient final String UC_NAME = "Caso de Uso 1";
+    private transient final String UC_DESCRIPTION = "First UC";
+    
+    //Input data for Project
+    private transient final int NUMBER_OF_ITERATIONS = 2;
+    private transient final int NUMBER_OF_WEEKS = 3;
+    private transient final String PROJECT_DESCRIPTION = "First project";
+    private transient final String PROJECT_NAME = "Project 1";
+    
+    //Input data for User
+    private transient final String NAME = "Pepe";
+    private transient final String PASSWORD = "1234";
+    private transient final String USERNAME = "pepe23";
+    private transient final String EMAIL = "pepe@pepe.com";
+    private transient final String SURENAME = "lopez";
+    private transient final String SECOND_SURENAME = "guti";
     
     @Before
     public void before() {
-        ProjectDAO projectDAO = AbstractDAOFactory.getFactory().getProjectDAO();
+        userDAO = AbstractDAOFactory.getFactory().getUserDAO();
+        projectDAO = AbstractDAOFactory.getFactory().getProjectDAO(); 
+        usecaseDAO = AbstractDAOFactory.getFactory().getUseCaseDAO();
         
         usecaseEjb = new UseCaseEjb();
-        usecase = new UseCaseEntity();
-        usecase.setName("UseCase 1");
-        usecase.setDescription("UseCase de ejemplo");
-        usecase.setProject(projectDAO.read(1));
+        
+        fill_userentity();
+        fill_projectentity();
+        fill_usecaseentity();      
+        
+        userDAO.create(user);                
+        projectDAO.create(project); 
     }
     
     @Test
-    public void updateTest(){
-        UseCaseDAO useCaseDAO = AbstractDAOFactory.getFactory().getUseCaseDAO();
-        useCaseDAO.create(usecase); // se guarda en la base de datos
+    public void updateTest(){        
+        usecaseDAO.create(usecase); // se guarda en la base de datos
         usecase.setDescription("UseCase de ejemplo modificado");
         usecase.setName("UseCase 1 Modificado");
         usecaseEjb.update(usecase);
-        UseCaseEntity usecase2 = useCaseDAO.read(usecase.getId()); // se obtiene de la BD
-        useCaseDAO.delete(usecase);
+        UseCaseEntity usecase2 = usecaseDAO.read(usecase.getId()); // se obtiene de la BD        
         assertTrue("Modified", usecase2.getDescription().equals("UseCase de ejemplo modificado") && usecase2.getName().equals("UseCase 1 Modificado"));
+    }
+    
+    @Test
+     public void createUseCase() {               
+        usecaseDAO.create(usecase);
+        usecaseEjb.create(usecase);       
+        String[] name = {"name"};
+        String[] values = {UC_NAME};        
+        UseCaseEntity recoveredUC = usecaseDAO.read(usecase.getId());        
+        assertEquals(recoveredUC.getName(), usecase.getName());
+        assertEquals(recoveredUC.getDescription(), usecase.getDescription());
+     }
+    
+    public void fill_userentity() {
+        this.user = new UserEntity();
+        this.user.setName(NAME);
+        this.user.setPassword(PASSWORD);
+        this.user.setUsername(USERNAME);
+        this.user.setEmail(EMAIL);
+        this.user.setSurename(SURENAME);
+        this.user.setSecondSurename(SECOND_SURENAME);
+    }
+
+    public void fill_projectentity() {
+        project = new ProjectEntity();
+        project.setChosenNumIteration(NUMBER_OF_ITERATIONS);
+        project.setDescription(PROJECT_DESCRIPTION);
+        project.setStartDate(new Date());
+        project.setEndDate(new Date());
+        project.setEstimatedNumIteration(NUMBER_OF_ITERATIONS);
+        project.setName(PROJECT_NAME);
+        project.setWeekNumIteration(NUMBER_OF_WEEKS);
+        project.setOwner(user);
+    }
+
+    public void fill_usecaseentity() {
+        usecase = new UseCaseEntity();
+        usecase.setName(UC_NAME);
+        usecase.setDescription(UC_DESCRIPTION);
+        usecase.setProject(project);
+    }
+    
+    @After
+    public void after() {                      
+        usecaseDAO.delete(usecase);
+        projectDAO.delete(project);
+        userDAO.delete(user);        
     }
 }
