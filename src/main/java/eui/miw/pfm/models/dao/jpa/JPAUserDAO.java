040ce2eaa0e3187f615b461167bb77430009fc3a
Fred Pena <f.ant.pena@gmail.com>
2014-05-06 09:55:09 +0200

    - Creacion y implementación del metodo findUser (UserDAO y JPAUserDAO)

--- src/main/java/eui/miw/pfm/models/dao/jpa/JPAUserDAO.java
+++ src/main/java/eui/miw/pfm/models/dao/jpa/JPAUserDAO.java
@@ -2,10 +2,19 @@ package eui.miw.pfm.models.dao.jpa;
 
 import eui.miw.pfm.models.dao.interfaces.UserDAO;
 import eui.miw.pfm.models.entities.UserEntity;
+import javax.persistence.Query;
 
-public class JPAUserDAO extends JPATransactionGenericDAO<UserEntity, Integer> implements UserDAO{
+public class JPAUserDAO extends JPATransactionGenericDAO<UserEntity, Integer> implements UserDAO {
 
     public JPAUserDAO() {
         super(UserEntity.class);
+    }
+
+    @Override
+    public UserEntity findUser(String nameUser, String password) {
+        Query query = entityManager.createQuery("SELECT u FROM UserEntity u WHERE u.username = ?1 AND u.password = ?2");
+        query.setParameter(1, nameUser);
+        query.setParameter(2, password);
+        return (UserEntity) query.getSingleResult();
     }
 }
--- src/main/java/eui/miw/pfm/models/entities/UserEntity.java
+++ src/main/java/eui/miw/pfm/models/entities/UserEntity.java
@@ -6,14 +6,11 @@
 package eui.miw.pfm.models.entities;
 
 import java.io.Serializable;
-import java.util.List;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
