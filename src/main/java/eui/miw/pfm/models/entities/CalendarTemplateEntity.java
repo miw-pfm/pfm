f5405caed13077878ab699c78568ef3682604a45
Jean Mubaied <mubaied.jm@gmail.com>
2014-05-08 12:41:08 +0200

    Modificacion de nombres de las tablas en las entidades.

--- src/main/java/eui/miw/pfm/models/entities/CalendarTemplateEntity.java
+++ src/main/java/eui/miw/pfm/models/entities/CalendarTemplateEntity.java
@@ -13,6 +13,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -22,6 +23,7 @@ import javax.validation.constraints.Size;
  * @author Jean Mubaied
  */
 @Entity
+@Table (name="calendartemplate")
 public class CalendarTemplateEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
