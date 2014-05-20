9cdbf45f8ffba688754c4542859c2489a59f4ff5
Manu AlvGar <manualvgar@gmail.com>
2014-05-20 18:09:53 +0200

    - Modificado IterationEntity cambiando Date por Calendar y haciendo type enumerable - Creación de enum TypeIteration - Realizado TestIteration

--- src/main/java/eui/miw/pfm/models/dao/interfaces/IterationDAO.java
+++ src/main/java/eui/miw/pfm/models/dao/interfaces/IterationDAO.java
@@ -3,6 +3,6 @@ package eui.miw.pfm.models.dao.interfaces;
 import eui.miw.pfm.models.dao.TransactionGenericDAO;
 import eui.miw.pfm.models.entities.IterationEntity;
 
-public interface IterationDAO extends TransactionGenericDAO<IterationEntity, Integer>{       
-    
-        }
+public interface IterationDAO extends TransactionGenericDAO<IterationEntity, Integer> {
+
+}
