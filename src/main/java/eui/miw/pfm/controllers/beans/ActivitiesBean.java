d58d7b22e9dab70ad5eb9d93a3d0474b0c09443a
jvillarb <jvillarb@hotmail.com>
2014-05-26 13:48:13 +0200

    Desarrollo vista activitiesDetailedAsignation

--- src/main/java/eui/miw/pfm/controllers/beans/ActivitiesBean.java
+++ src/main/java/eui/miw/pfm/controllers/beans/ActivitiesBean.java
@@ -14,27 +14,35 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedProperty;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
 import javax.inject.Named;
+import org.primefaces.event.CellEditEvent;
 
 /**
  *
  * @author Jean Mubaied
  * @author Jose Mª Villar
  */
-@RequestScoped
+@ViewScoped
 @Named
 public class ActivitiesBean extends Bean implements Serializable {
-    
+
     private static final long serialVersionUID = 1L;
-    
+
     private ProjectEntity project;
+    private transient ActivityEntity activity;
+    private transient List<SubActivityEntity> subActivities;
     private static final Logger LOGGER = Logger.getLogger(ProjectConfBean.class.getName());//NOPMD
+    private String prueba;
+    
 
     @ManagedProperty(value = "#{iterationBean}")
     private final transient IterationBean iterationBean = new IterationBean();
-    
-    public ActivitiesBean() {        
+
+    public ActivitiesBean() {
         super();
         try {
             this.project = ((ProjectEntity) sessionMap.get("project"));
@@ -42,28 +50,34 @@ public class ActivitiesBean extends Bean implements Serializable {
             LOGGER.warning("No session exist");
         }
 
-        this.project = AbstractDAOFactory.getFactory().getProjectDAO().read(project.getId());        
-    }    
-    
-    public List<SubActivityEntity> getSubActivities() {
-        ActivitiesEjb activityEjb;
-        activityEjb = new ActivitiesEjb();        
-        return activityEjb.obtainAllSubActivities();
-    }
-    
-    public List<SubActivityEntity> getSubActivities(final ActivityEntity activity) {
-        ActivitiesEjb activityEjb;
-        activityEjb = new ActivitiesEjb();                
-        return activityEjb.obtainSubActivities(activity);
-    }
-    
-    public List<ActivityEntity> getActivities() {
-        return new ActivitiesEjb().obtainAllActivities();  
-    }
+        this.project = AbstractDAOFactory.getFactory().getProjectDAO().read(project.getId());
+        this.activity = AbstractDAOFactory.getFactory().getActivityDAO().read(1);
         
+        ActivitiesEjb activityEjb;
+        activityEjb = new ActivitiesEjb();
+        this.subActivities = activityEjb.obtainSubActivities(activity);        
+    }
+
+    
+    public ActivityEntity getActivity() {
+        return activity;
+    }
+
+    public void setActivity(final ActivityEntity activity) {
+        this.activity = activity;
+    }
+
+    public List<SubActivityEntity> getSubActivities() {
+        return subActivities;
+    }
+
+    public void setSubActivities(final List<SubActivityEntity> subActivities) {
+        this.subActivities = subActivities;
+    }
+
     public ProjectEntity getProject() {
         return project;
-    }      
+    }
 
     public void setProject(final ProjectEntity project) {
         this.project = project;
@@ -72,5 +86,26 @@ public class ActivitiesBean extends Bean implements Serializable {
     public IterationBean getIterationBean() {
         return iterationBean;
     }
-       
+
+    public List<SubActivityEntity> getSubActivitiesByActivity(final ActivityEntity activity) {
+        ActivitiesEjb activityEjb;
+        activityEjb = new ActivitiesEjb();
+        return activityEjb.obtainSubActivities(activity);
+    }
+
+    public List<ActivityEntity> getActivities() {
+        return new ActivitiesEjb().obtainAllActivities();
+    }
+    
+    public void onCellEdit(final CellEditEvent event) {
+//        final Object oldValue = event.getOldValue();
+//        final Object newValue = event.getNewValue();
+        
+        System.out.println("---------------------------");
+        
+//        if(newValue != null && !newValue.equals(oldValue)) {
+//            final FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Cell Changed", "Old: " + oldValue + ", New:" + newValue);
+//            FacesContext.getCurrentInstance().addMessage(null, msg);
+//        }
+    }    
 }
