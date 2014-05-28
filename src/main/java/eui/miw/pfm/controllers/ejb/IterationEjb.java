be2c419d8b7c5a5382cdedf36faa79ec525f6495
HWilliamRS <HWilliamRS@yahoo.com>
2014-05-28 23:40:05 +0200

    Integrar Mostrar Unidades de Trabajo con el codigo de cada actividad en bloques individuales por iteración Part2

--- src/main/java/eui/miw/pfm/controllers/ejb/IterationEjb.java
+++ src/main/java/eui/miw/pfm/controllers/ejb/IterationEjb.java
@@ -15,7 +15,11 @@ import java.util.List;
  * @author Manuel Alvarez
  */
 public class IterationEjb {
-
+    
+    public IterationEntity obtainIteration(final Integer iterationID){
+        return AbstractDAOFactory.getFactory().getIterationDAO().read(iterationID);
+    }
+    
     public void create(final IterationEntity iterationEntity) {
         AbstractDAOFactory.getFactory().getIterationDAO().create(iterationEntity);
     }
