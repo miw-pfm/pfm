399b3ac99fd0a5dc4c7a123a933f0c3594f566ba
Jean Mubaied <mubaied.jm@gmail.com>
2014-06-05 12:58:23 +0200

    Implemetar metodos crear y actualizar en Bean "TargetBean"

---
+++ src/test/java/eui/miw/pfm/controllers/ejb/TestTarget.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eui.miw.pfm.controllers.ejb;
+
+import eui.miw.pfm.models.entities.TargetEntity;
+
+/**
+ *
+ * @author bk0820
+ */
+public class TestTarget {
+    private transient TargetEntity target;
+    private transient TargetEjb targetEjb;
+    
+}
