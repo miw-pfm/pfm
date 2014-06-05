bdb6dc94f09402b3eda3dbca0501ecaf80cd6240
Jean Mubaied <mubaied.jm@gmail.com>
2014-06-05 12:17:24 +0200

    Creacion de los archivos "TargetBean" y "TargetEjb"

---
+++ src/main/java/eui/miw/pfm/controllers/ejb/TargetEjb.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eui.miw.pfm.controllers.ejb;
+
+import eui.miw.pfm.models.dao.AbstractDAOFactory;
+import eui.miw.pfm.models.entities.SubActivityEntity;
+import java.util.List;
+
+/**
+ *
+ * @author Jean Mubaied
+ */
+public class TargetEjb {
+    
+  
+    
+}
