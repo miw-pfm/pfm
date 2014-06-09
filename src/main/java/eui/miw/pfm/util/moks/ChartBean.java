42539eb95f4c69dfa00b334ebfae81811f85aa99
bk0820 <bk0820@192.168.7.28>
2014-06-09 14:06:29 +0200

    Desarrollando ChartProgressAbstract
    Jose Mª Villar Bógalo
    Jean Mubaied

--- src/main/java/eui/miw/pfm/util/moks/ChartBean.java
+++ src/main/java/eui/miw/pfm/util/moks/ChartBean.java
@@ -32,22 +32,27 @@ public class ChartBean extends Bean implements Serializable {
 
     private static final long serialVersionUID = 1L;
     private CartesianChartModel categoryModel;
+    private CartesianChartModel categoryModelPAbs;
 
-    private transient final ChartSeries percentIdentification = new ChartSeries();
-    private transient final ChartSeries percentSpecification = new ChartSeries();
-    private transient final ChartSeries percentDesign = new ChartSeries();
-    private transient final ChartSeries percentImplementation = new ChartSeries();
-    private transient final ChartSeries percentTest = new ChartSeries();
+    
+    private transient final ChartSeries pIdentification = new ChartSeries();
+    private transient final ChartSeries pSpecification = new ChartSeries();
+    private transient final ChartSeries pDesign = new ChartSeries();
+    private transient final ChartSeries pImplementation = new ChartSeries();
+    private transient final ChartSeries pTest = new ChartSeries();
 
     private transient ProjectEntity project;
     private static final Logger LOGGER = Logger.getLogger(ProjectConfBean.class.getName());//NOPMD
 
     public ChartBean() {
+        super();
         try {
             this.project = ((ProjectEntity) sessionMap.get("project"));
         } catch (Exception e) {
             LOGGER.warning("No session exist");
         }
+        
+        createCategoryModelProgressAbstract();
         createCategoryModel();
     }
 
@@ -93,26 +98,50 @@ public class ChartBean extends Bean implements Serializable {
         this.categoryModel = categoryModel;
     }
 
-    public void createCategoryModelProgressAbstract() {
+    public CartesianChartModel getCategoryModelPAbs() {
+        return categoryModelPAbs;
+    }
+
+    public void setCategoryModelPAbs(final CartesianChartModel categoryModelPAbs) {
+        this.categoryModelPAbs = categoryModelPAbs;
+    }
+    
+    private void createCategoryModelProgressAbstract() {
         final List<IterationEntity> iterations = new IterationEjb().getAllIterations(project);
-        final List<DisciplineEntity> discipline = new DisciplineEjb().findAll();
-        final ProgressDetailEjb progressDetailEjb = new ProgressDetailEjb();
-        
-        this.percentIdentification.setLabel("Identification");
-        this.percentSpecification.setLabel("Specification");
-        this.percentDesign.setLabel("Design");
-        this.percentImplementation.setLabel("Implementatio");
-        this.percentTest.setLabel("Test");
-        
+        final List<DisciplineEntity> discipline = new DisciplineEjb().findAll(); //NOPMD
+        final ProgressDetailEjb progressDetailEjb = new ProgressDetailEjb(); //NOPMD
+
+        this.categoryModelPAbs = new CartesianChartModel();
+                
+        this.pIdentification.setLabel("Identification");
+        this.pSpecification.setLabel("Specification");
+        this.pDesign.setLabel("Design");
+        this.pImplementation.setLabel("Implementatio");
+        this.pTest.setLabel("Test");
 
         for (IterationEntity iterationEntity : iterations) {
             for (DisciplineEntity disciplineEntity : discipline) {
-                this.percentSpecification.set(iterationEntity.getCodeIteration(), progressDetailEjb.getSumTotalProgressDetail(iterationEntity, disciplineEntity));
-                this.percentDesign.set(iterationEntity.getCodeIteration(), progressDetailEjb.getSumTotalProgressDetail(iterationEntity, disciplineEntity));
-                this.percentImplementation.set(iterationEntity.getCodeIteration(), progressDetailEjb.getSumTotalProgressDetail(iterationEntity, disciplineEntity));
-                this.percentTest.set(iterationEntity.getCodeIteration(), progressDetailEjb.getSumTotalProgressDetail(iterationEntity, disciplineEntity));
-                
+                switch (disciplineEntity.getName()) {
+                    case "Specification":
+                        this.pSpecification.set(iterationEntity.getCodeIteration(), progressDetailEjb.getSumTotalProgressDetail(iterationEntity, disciplineEntity));
+                        break;
+                    case "Design":
+                        this.pDesign.set(iterationEntity.getCodeIteration(), progressDetailEjb.getSumTotalProgressDetail(iterationEntity, disciplineEntity));
+                        break;
+                    case "Implementation":
+                        this.pImplementation.set(iterationEntity.getCodeIteration(), progressDetailEjb.getSumTotalProgressDetail(iterationEntity, disciplineEntity));
+                        break;
+                    case "Test":
+                        this.pTest.set(iterationEntity.getCodeIteration(), progressDetailEjb.getSumTotalProgressDetail(iterationEntity, disciplineEntity));
+                        break;
+                    default: break;
+                }
             }
         }
+        
+        categoryModelPAbs.addSeries(pSpecification);
+        categoryModelPAbs.addSeries(pDesign);
+        categoryModelPAbs.addSeries(pImplementation);
+        categoryModelPAbs.addSeries(pTest);        
     }
 }
